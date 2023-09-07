cfa6a2ad568e2d3c8d850cf22d26aedaf2d493dd
RAKESH KUMAR GAUTAM <115333787+RKG071204@users.noreply.github.com>
2023-09-07 15:34:43 +0530

    Update Home.js

--- Home.js
+++ Home.js
@@ -1,10 +1,24 @@
 import React from 'react';
+import { Link } from 'react-router-dom'; // Import Link from React Router
 
 const Home = () => {
   return (
     <div className="content-main">
-      <h2>Home</h2>
-      <p>This is the Home page content.</p>
+      <div className="home-header">
+        <h2>Home</h2>
+        <button className="add-button">ADD</button>
+      </div>
+      <div className="card-container">
+        <Link to="/projects" className="card project-card">
+          <p>Projects</p>
+        </Link>
+        <Link to="/others" className="card other-card">
+          <p>Others</p>
+        </Link>
+        <Link to="/others" className="card other-card">
+          <p>Others</p>
+        </Link>
+      </div>
     </div>
   );
 };
